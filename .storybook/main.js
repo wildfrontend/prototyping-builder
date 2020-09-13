0b8d5e95f78b2c7f1774b0cdbe132c9b73c00db0
wildfrontend <louis0420tw@gmail.com>
2020-09-14 03:11:51 +0800

    feat(storybook): add storybook

---
+++ .storybook/main.js
@@ -0,0 +1,12 @@
+require("core-js/stable");
+require("regenerator-runtime/runtime");
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ]
+}
